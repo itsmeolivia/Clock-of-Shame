1aa0b55275d2635b44f16de01f0c61f2c2757711
Olivia Nordquist <nolivia@umich.edu>
2015-07-01 20:26:09 -0700

    change of plans

--- eventPage.js
+++ eventPage.js
@@ -1,9 +1,10 @@
-/*chrome.history.getVisits({url: "facebook.com"}, function (visits) {
-  console.log(visits)
-});
-*/
 chrome.webNavigation.onDOMContentLoaded.addListener(function (data){
-  if (data.frameId !== 0 || !(/^https?:\/\/(?:www\.)?facebook\.com/i).test(data.url)) 
+  if (data.frameId !== 0 || !(/^https?:\/\/(?:www\.)?facebook\.com/i).test(data.url))
     return;
-  console.log(data);
+  chrome.history.getVisits({url: "facebook.com"}, function (visits) {
+    console.log(visits)
+  });
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
 });
--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-alert("hello nerd");
+alert("Hello evan");
