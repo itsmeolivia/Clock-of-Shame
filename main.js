e083767d7f432dc213ac661a54b861f89d566ae9
Olivia Nordquist <nolivia@umich.edu>
2015-07-01 21:15:47 -0700

    overly

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert("Hello evan");
+var element = $("<div></div>");
+element.css({
+  backgroundColor: "red",
+  position: "fixed",
+  top: "0px",
+  left: "0px",
+  width: "100%",
+  height: "100%"
+});
+$("body").append(element);
